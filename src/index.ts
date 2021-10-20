d0f9365c4a0fcc29c1393e92cfa8996af5081fc7
Matheus Medeiros da Silva <65987506+Matheusmeed@users.noreply.github.com>
2021-10-19 23:22:49 -0300

    Deploy no Heroku

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
-import * as express from 'express';
-import * as bodyParser from 'body-parser';
-import * as cors from 'cors';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
@@ -12,4 +12,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333, () => {
+    console.log('Servidor em Execução');
+});
